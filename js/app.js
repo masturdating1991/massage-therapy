758b700f86ea989914d850e2454662dec95daa3a
masturdating1991 <aligodosi@gmail.com>
2022-06-04 15:43:09 +0430

    people say slider

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
-let swiper = new Swiper(".slider", {
+let swiper1 = new Swiper(".slider", {
     loop: true,
     autoplay: {
         delay: 2500,
     },
 
+});
+
+
+let swiper2 = new Swiper(".quote-slider", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
